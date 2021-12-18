6ba6ce82c54eead65119a8b846c4b34e0f08da46
MahletM <Mahletmelesse48@gmail.com>
2021-12-18 21:53:31 +0300

    pause menu
    added pause menu to level 3.

---
+++ Assets/C#/PauseMenu.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject remover;
+    public GameObject option;
+    
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) && option.activeInHierarchy == false)
+        {
+        if(isPaused)
+        {
+            Continue();
+        } else
+        {
+            pauseM();
+        }
+        }  
+    }
+
+    public void Continue()
+    {
+        remover.SetActive(true);
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+    public void pauseM()
+    {
+        remover.SetActive(false);  
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+    public void Exit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+    
+}
